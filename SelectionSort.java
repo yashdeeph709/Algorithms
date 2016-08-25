432cb1c4e5b11f15545443d30f571a05ca3df905
yashdeep hinge <yashdeep.hinge@sap.com>
2016-08-25 18:20:26 +0530

    Selection Sort Closes yashdeeph709/Algorithms#1

---
+++ SelectionSort.java
@@ -0,0 +1,17 @@
+class SelectionSort{
+	public static void main(String args[]){
+		int[] array={10,12,9,4,6,456,498,1,56};
+		for(int i=0;i<array.length;i++){
+			for(int j=i;j<array.length;j++){
+				if(array[i]>array[j]){
+					int temp=array[i];
+					array[i]=array[j];
+					array[j]=temp;
+				}
+			}
+		}
+		for(int i=0;i<array.length;i++){
+			System.out.println(array[i]);
+		}
+	}
+}
