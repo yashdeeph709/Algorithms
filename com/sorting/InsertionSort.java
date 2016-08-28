df85c2739069366338d285fb1fcf2673b22c4d3f
yashdeeph709 <yashdeep.hinge@sap.com>
2016-08-28 10:33:53 +0530

    Packaging added and Insertion got implemented!

--- com/sorting/InsertionSort.java
+++ com/sorting/InsertionSort.java
@@ -1,4 +1,6 @@
-class SelectionSort{
+package com.sorting;
+
+class InsertionSort{
 	public static void main(String args[]){
 		int[] array={10,12,9,4,6,456,498,1,56};
 		for(int i=0;i<array.length;i++){
