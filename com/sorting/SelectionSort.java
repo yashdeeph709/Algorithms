e152ec3455b209531358077547e0bd96cbcf3229
yashdeeph709 <yashdeep.hinge@sap.com>
2016-08-29 11:03:11 +0530

    Insertion and Selection closes #1,#2

--- com/sorting/SelectionSort.java
+++ com/sorting/SelectionSort.java
@@ -3,17 +3,22 @@ package com.sorting;
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		int[] array={12,23,2,3452,34,4,435,234,234,24,3};
+		int[] array={10,12,9,4,6,456,498,1,56};
 		
-		for(int i=0;i<array.length;i++){
+		for(int i=0;i<array.length-1;i++){
+			int jMin=i;
 			for(int j=i+1;j<array.length;j++){
-				if(array[j]>array[i]){
-					int temp=array[i];
-					array[i]=array[j];
-					array[j]=array[i];
+				if(array[j]<array[jMin]){
+					jMin=j;
 				}
 			}
+			if(jMin != i){
+				array[i]=array[jMin]+array[i];
+				array[jMin]=array[i]-array[jMin];
+				array[i]=array[i]-array[jMin];
+			}
 		}
+		
 		for(int i=0;i<array.length;i++){
 			System.out.println(array[i]);
 		}
