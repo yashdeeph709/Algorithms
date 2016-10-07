c0c1c6ca8cbff3dddf23b947aa3e98a083b62ec8
yashdeep hinge <C5244128@PNQN50902942A.apj.global.corp.sap>
2016-10-08 02:08:29 +0530

    Two More solved in practice for implementation

---
+++ HackerEarth/src/com/hackerearth/BennyAndBalls.java
@@ -0,0 +1,37 @@
+package com.hackerearth;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BennyAndBalls {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int noOfTestcases = sc.nextInt();
+		while(noOfTestcases-->0){
+			
+			int N=sc.nextInt(),N1=N;
+			int baskets[]=new int[N];
+			int oldbackets[]=new int[N];
+			while(N-->0){
+				baskets[baskets.length-N-1]=sc.nextInt();
+				oldbackets[baskets.length-N-1]=baskets[baskets.length-N-1];
+			}
+			
+			long x1 = sc.nextLong();
+			int a = sc.nextInt(),b = sc.nextInt();
+			int t = sc.nextInt();
+			int result=0;
+			while(--t>=0){
+				int temp=(int)x1;
+				if(--baskets[temp]==0){
+					result++;
+					baskets[temp]=oldbackets[temp];
+				}
+				x1=(1l*a*x1+b)%N1;
+			}
+			System.out.println(result);
+		}
+	}
+
+}
---
+++ HackerEarth/src/com/hackerearth/PrintHackerEarth.java
@@ -0,0 +1,36 @@
+package com.hackerearth;
+
+import java.util.Scanner;
+
+public class PrintHackerEarth {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Scanner sc1 = new Scanner(System.in);
+		int length = sc.nextInt();
+		String str = sc1.nextLine();
+		int[] freq=new int[26];
+		int[] map={0,2,4,7,10,17,19};
+		for(int i=0;i<str.length();i++){
+			freq[(int)str.charAt(i)-97]++;
+		}
+		int mini=Integer.MAX_VALUE;
+		for(int i=0;i<map.length;i++){
+			if(i==0 || i==2 || i==5 || i==3){
+				mini=min(mini,freq[map[i]]/2);
+			}else if(i==1 || i==4 || i==6){
+				mini=min(mini, freq[map[i]]);
+			}
+		}
+		System.out.println(mini);
+	}
+	public static int min(int a,int b){
+		if(a>b){
+			return b;
+		}
+		if(a<b){
+			return a;
+		}
+		return a;
+	}
+}
