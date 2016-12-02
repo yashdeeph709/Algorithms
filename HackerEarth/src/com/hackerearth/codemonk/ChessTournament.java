25b4db71968a2075d1f2b6657f5b7caf34e818f2
yashdeep hinge <C5244128@PNQN50902942A.apj.global.corp.sap>
2016-12-02 13:00:45 +0530

    Chess Tournament is done working great 0.72seconds in average

--- HackerEarth/src/com/hackerearth/codemonk/ChessTournament.java
+++ HackerEarth/src/com/hackerearth/codemonk/ChessTournament.java
@@ -3,36 +3,35 @@ package com.hackerearth.codemonk;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.FileReader;
 
 public class ChessTournament{
+	static int[][] array;
+	static int counter=0;
 	public static void main(String args[]) throws IOException{
 		InputStreamReader is=new InputStreamReader(System.in);
-		BufferedReader br=new BufferedReader(new FileReader("C:\\Users\\c5244128\\Desktop\\input-d9c39df.txt"));
-//		BufferedReader br=new BufferedReader(is);
+		BufferedReader br=new BufferedReader(is);
+	//	BufferedReader br=new BufferedReader(new FileReader("C:\\Users\\c5244128\\Desktop\\HackerZone\\HackerEarth\\src\\com\\hackerearth\\codemonk\\sorting\\chesstournament.txt"));
 
 		int N=Integer.parseInt(br.readLine());
 		int size=(int)Math.pow(2,N);
-		int[][] array=new int[size][size];
-		int[] winnings=new int[size];
+		array=new int[size][size];
 		String[] numbers;
 		for(int i=1;i<size;i++){
 			numbers=br.readLine().split(" ");
-			int winningstemp=0;
 			for(int j=0;j<i;j++){
 				array[i][j]=Integer.parseInt(numbers[j]);
-				if(array[i][j]!=0){
-					winningstemp++;
-				}
-			}
-			if(winningstemp<=N){
-				winnings[i]=winningstemp;
-			}else{
-				winnings[i]=0;
 			}
 		}
-		for(int i=0;i<winnings.length;i++){
-			System.out.println(i+" :"+winnings[i]);
+		System.out.println(findWinner(0,array.length-1)+1);
+	}
+	public static int findWinner(int a,int b){
+		if(b-a==1){
+			return array[b][a]==0?a:b;
+		}else{
+			int mid=((b-a)/2)+a;
+			int winner1=findWinner(a, mid);
+			int winner2=findWinner(mid+1, b);
+			return array[winner2][winner1]==0?winner1:winner2;
 		}
 	}
 }
