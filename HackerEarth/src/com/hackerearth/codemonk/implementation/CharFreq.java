b49223c96275a596bbaa5b583ee9d285fe5b23c9
yashdeep hinge <C5244128@PNQN50902942A.apj.global.corp.sap>
2016-12-05 12:34:45 +0530

    Moved to apt package

--- HackerEarth/src/com/hackerearth/codemonk/implementation/CharFreq.java
+++ HackerEarth/src/com/hackerearth/codemonk/implementation/CharFreq.java
@@ -9,9 +9,27 @@ public class CharFreq{
 		char c=br.readLine().charAt(0);
 		int n=Integer.parseInt(br.readLine());
 		int positions[]=new int[str.length()];
-		
+		int counter=-1,maxFreq=0,maxPosition=0;
+		if(str.length()==n){
+			System.out.println(str.lastIndexOf(c+"")+1);
+			return;
+		}
+		for(int i=0;i<=str.length()-n+1;i++){
+			String sub=str.substring(i,i+n-1);
+			int freq=0;
+			System.out.println(sub);
+			for(int j=0;j<sub.length();j++){
+				if(sub.charAt(j)==c){
+					freq++;
+				}
+			}
+			if(freq>=maxFreq){
+				maxFreq=freq;
+				maxPosition=i+n-1;
+			}
+		}
 		if(n!=1){
-			System.out.println(maxPos);
+			System.out.println(maxPosition);
 		}else{
 			System.out.println(-1);
 		}	
